c10857f7f7da4f56d9329ca56e946fbb39f877f5
Samuel-Fiais <samuel.fiaissf1@gmail.com>
2022-02-01 08:51:42 -0300

    Updated manager class e manager tests

--- src/main/kotlin/one/digitalinovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,8 +3,9 @@ package one.digitalinovation.digionebank.testes
 import one.digitalinovation.digionebank.Gerente
 
 fun main() {
-    val edmundo = Gerente("Edmundo Fiais Pereira", "123.456.789-10", 6350.0)
-
+    val edmundo = Gerente("Edmundo Fiais Pereira", "123.456.789-10", 6350.0, "12345")
     ImprimeRelatorioFuncionario.imprime(edmundo)
+
+    TesteAutenticacao().autentica(edmundo)
 }
 
