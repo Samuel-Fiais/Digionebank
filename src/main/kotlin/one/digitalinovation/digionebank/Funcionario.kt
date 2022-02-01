5ae31a9a62a4d0accec58f875b6761fcac354a92
Samuel-Fiais <samuel.fiaissf1@gmail.com>
2022-01-31 22:24:28 -0300

    update employee class

--- src/main/kotlin/one/digitalinovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Funcionario.kt
@@ -5,7 +5,14 @@ import java.math.BigDecimal
 abstract class Funcionario(
     nome: String,
     cpf: String,
-    val salario: BigDecimal
+    var salario: Double
 ): Pessoa(nome, cpf) {
-    abstract fun calculoAuxilio();
+    protected abstract fun calculoAuxilio(): Double
+
+    override fun toString(): String = """
+        Nome: $nome
+        CPF: $cpf
+        Salário: R$ $salario
+        Auxílio: R$ ${calculoAuxilio()}
+    """.trimIndent()
 }
