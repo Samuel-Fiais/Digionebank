3ee05bcac0e3da548eb7e4c705b113ab6a25d998
Samuel-Fiais <samuel.fiaissf1@gmail.com>
2022-01-31 21:22:49 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -1,19 +1,6 @@
 package one.digitalinovation.digionebank
 
-class Pessoa {
-    var nome: String = "Samuel"
-    var cpf: String = "166.700.736-07"
-        private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val samuel = Pessoa()
-
-    println(samuel.nome)
-    println(samuel.cpf)
-    println(samuel.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
